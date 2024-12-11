0c4f1173de74bd42bea8f56c128406047ab0124f
Mohammed Abir <153712202+Abir-rs@users.noreply.github.com>
2024-12-11 11:13:56 +0600

    Update help.js

--- Priyansh/commands/help.js
+++ Priyansh/commands/help.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "help",
 	version: "1.0.3",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "",
 	description: "Beginner's Guide",
 	commandCategory: "system",
 	usages: "[Command Name]",
